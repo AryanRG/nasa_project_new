114a81f80bc86f67893b9f0564b36ab7da1af8fa
Ryem8 <aryanrganjoo@gmail.com>
2021-05-26 16:23:59 +0530

    enter

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch ={
-    mission : 'Kepler Exploration X',       //name
-    rocket : 'Explorer IS1',                             //rockets.name
-    launchDate : new Date('December 27. 2030'),     //date_local
-    target : 'Kepler-442 b',                        //not applicable
-    customers : ['ZTM', 'NASA'],                     //payloads.customers
-    upcoming : true,                                //upcoming
-    success: true,                                  //success
-    flightNumber : 101,                     //flight_number
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
 async function populateLaunches(){
@@ -108,9 +95,12 @@ async function getLatestFlightNumber(){
     return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches(){
+async function getAllLaunches(skip, limit){
     return await launchesDatabase.
-    find({},{ '__v': 0 , '_id' :0 });
+    find({},{ '__v': 0 , '_id' :0 })
+    .sort({ flightNumber : 1})
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch){   
